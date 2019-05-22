a010e7cedd0ef2a7d4d055933106cef7cf5e6734
Ayush Shrivastava <ayush.shrivastava2901@gmail.com>
2019-05-22 16:35:06 +0530

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -2,7 +2,7 @@
 
 $dbServerName = "localhost";
 $dbUserName = "root";
-$dbPassword = "Piyush*1";
+$dbPassword = "";
 $dbName = "log_pro";
 
 $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
